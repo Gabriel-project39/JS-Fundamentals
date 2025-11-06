1e7ff6497979029ebf5ef753143c63ca84006972
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2025-11-06 23:59:34 +0300

    made some Change

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,3 @@
 const argsCount = process.argv.length - 2;
 
-if (argsCount === 0) {
-    console.log("0 argument");
-} else if (argsCount === 1) {
-    console.log("1 argument");
-} else {
-    console.log(`${argsCount} arguments`);
-}
+console.log(`${argsCount} ${argsCount === 1 ? "argument" : "arguments"}`);
