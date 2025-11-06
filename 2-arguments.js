c4199ffaf77cddfc6ec80fb5208936deedfd625e
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2025-11-07 00:08:45 +0300

    changes

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,3 +1,10 @@
 const argsCount = process.argv.length - 2;
 
-console.log(`${argsCount} ${argsCount === 1 ? "argument" : "arguments"}`);
+if (argsCount === 0) {
+  console.log("0 argument");
+} else if (argsCount === 1) {
+  console.log("1 argument");
+} else {
+  console.log(`${argsCount} arguments`);
+}
+
