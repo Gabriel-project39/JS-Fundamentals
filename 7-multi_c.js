c9840686b8e4c88c56ed0cedbe8905d461ebe5ff
Gabriel Kimanthi <gabrielkimanthi39@gmail.com>
2025-11-07 01:12:25 +0300

    7 i love c

---
+++ 7-multi_c.js
@@ -0,0 +1,14 @@
+// 7-multi_c.js
+
+const arg = process.argv[2];
+const count = parseInt(arg);
+
+if (isNaN(count) || count <= 0) {
+    console.log("Missing number of occurrences");
+} else {
+    let i = 0;
+    while (i < count) {
+        console.log("C is fun");
+        i++;
+    }
+}
